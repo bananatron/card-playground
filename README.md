8b609ffcdd37e632ceec434992a4198dd421952d
Spenser Coke <work@spensercoke.com>
2016-12-18 09:35:04 -0800

    Let's do it

---
+++ README.md
@@ -0,0 +1,1 @@
+Test your multiplayer card game concepts.
---
+++ controls.js
@@ -0,0 +1,98 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAWWbSgUegnZ5OR47iwCaj8NmMdnjBjgtg",
+  authDomain: "card-tester.firebaseapp.com",
+  databaseURL: "https://card-tester.firebaseio.com",
+  storageBucket: "card-tester.appspot.com",
+  messagingSenderId: "1023196840212"
+};
+firebase.initializeApp(config);
+
+$('body').on('click', '.card', function(event){
+  $(event.currentTarget).toggleClass('is-facedown');
+});
+
+$('[data-action="new-card"]').on('click', function(event){
+  var player = $(event.currentTarget).attr('data-player');
+  var playerTop = $('.' + player).css('top');
+  uploadMessage(player + ' drew a card. ' + Math.floor(Math.random()*20));
+
+  if (player === 'p1'){
+    var cardsRef = firebase.database().ref('cards').push({
+      owner:'p1',
+      left: 0,
+      top: 0
+    });
+  } else {
+    var cardsRef = firebase.database().ref('cards').push({
+      owner:'p2',
+      left: 0,
+      top: playerTop
+    });
+  }
+});
+
+
+var createCard = function(card_id, card_data){
+  var $card = $('.card-template').clone();
+
+  $card.addClass('card');
+  $card.removeClass('card-template');
+  $card.attr('data-id', card_id);
+  $card.attr('data-owner', card_data.owner);
+  $card.draggable({
+    stop: function(e){
+      var cardRef = firebase.database().ref('cards/' + card_id);
+      cardRef.update({
+        top: $card.css('top'),
+        left: $card.css('left')
+      })
+    }
+  });
+  $card.css({left: card_data.left, top: card_data.top});
+  $('body').append($card);
+
+  $card.show();
+};
+
+// Listens for new cards joining the fray
+var globalCardListener = function() {
+  var allCardsRef = firebase.database().ref('/cards');
+
+  // When cards are changed
+  allCardsRef.on('value', function(new_cards) {
+    $('.card').remove(); // Remove all cards
+
+    // Re-add at their locations
+    Object.keys(new_cards.val()).forEach(function(card_id){
+      createCard(card_id, new_cards.val()[card_id]);
+    })
+  });
+};
+globalCardListener();
+
+
+var postMessage = function(message){
+  var $modal = $('.message-modal');
+  $modal.text(message);
+  $modal.removeClass('is-hidden');
+  setTimeout(function(){
+    $modal.addClass('is-hidden');
+  }, 1200)
+};
+var uploadMessage = function(msg){
+  var messageRef = firebase.database().ref('/message');
+  messageRef.set({
+    message: msg
+  })
+};
+
+var messageListener = function(){
+  var messageRef = firebase.database().ref('/message');
+  messageRef.on('value', function(message_snap) {
+    postMessage(message_snap.val().message);
+  });
+};
+messageListener();
+
+console.log('Controls loaded...');
