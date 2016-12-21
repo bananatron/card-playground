6cddeb720a611e7d2197c213a133b16fb7fe3c0a
Spenser Coke <work@spensercoke.com>
2016-12-20 17:25:05 -0800

    Add cards

---
+++ cards.js
@@ -0,0 +1,18 @@
+var card = {
+  name: 'lol',
+  image: 'card1.png',
+}
+
+var cards = {
+  'red_ooze': {
+    image: 'red_ooze.png'
+  }
+
+}
+
+
+var deck = [
+  'green_ooze',
+  'red_ooze',
+  'red_ooze'
+]
